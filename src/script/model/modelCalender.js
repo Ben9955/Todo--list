65b6ca5129004dfb2cfb0c9df8da760aac90bab2
Ben9955 <109614270+Ben9955@users.noreply.github.com>
2023-03-25 03:17:22 +0000

    Update modelCalender.js

--- src/script/model/modelCalender.js
+++ src/script/model/modelCalender.js
@@ -20,15 +20,7 @@ class ModelCalender {
   currentMonth = this.date.getMonth();
   currentYear = this.date.getFullYear();
   months = months;
-  //   lastDateMonth = this._calcLastDateMonth();
 
-  //   _calcLastDateMonth() {
-  //     this.lastDateMonth = new Date(
-  //       this.currentYear,
-  //       this.currentMonth + 1,
-  //       0
-  //     ).getDate();
-  //   }
 }
 
 export default new ModelCalender();
